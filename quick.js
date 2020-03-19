9fb29999ad4afb64cfa6e3930c7a4dd681de28c3
qingqingye <yejj@shanghaitech.edu.cn>
2020-03-19 22:51:38 +0800

    update my work

---
+++ quick.js
@@ -0,0 +1,17 @@
+var imgSrcArr = ['pictures/Gan/abs0.png','pictures/Gan/abs1.png','pictures/Gan/abs2.png','pictures/Gan/abs3.png',
+    'pictures/Gan/sim0.png','pictures/Gan/sim1.png','pictures/Gan/sim2.png','pictures/Gan/sim3.png',
+'pictures/Gan/imp0.png','pictures/Gan/imp1.png','pictures/Gan/imp2.png','pictures/Gan/imp3.png'];
+var imgWrap=[];
+
+
+function preload(arr){
+    alert("123");
+    for (var i=0; i<arr.length;i++){
+        imgWrap[i]=new Image();
+        imgWrap[i].src=arr[i];
+    }
+}
+
+$(function(){
+preload(imgSrcArr);
+})
